a08465575d4763a007b9b7e13126d71bc0679f8b
Bell <86065111+Bell-O@users.noreply.github.com>
2021-07-06 18:20:28 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,30 @@
+clear
+
+pkg install git -y
+
+apt-get upgrade -y
+
+apt-get install python3 -y
+
+apt-get install figlet
+
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+
+cd $HOME 
+
+cd hakFB
+
+mv bash.bashrc /data/data/com.termux/files/usr/etc
+
+cp f.sh /data/data/com.termux/files/usr/etc
+
+cp boby.py /data/data/com.termux/files/usr/etc
+
+rm setup.sh
+
+figlet Install Success
+
+bash
+
+
+
